f5fa033e015c063402f94384456282db284449e0
Nathan Schneider <schne165@purdue.edu>
2023-07-25 16:39:27 -0500

    date format

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -7,7 +7,7 @@ const router = require('express').Router()
 router.post('/add-income', addIncome)
     .get('/get-incomes', getIncomes)
     .delete('/delete-income/:id', deleteIncome)
-    .post('/add-expenses', addExpense)
+    .post('/add-expense', addExpense)
     .get('/get-expenses', getExpense)
     .delete('/delete-expense/:id', deleteExpense)
 
--- frontend/src/Components/IncomeItem/IncomeItem.js
+++ frontend/src/Components/IncomeItem/IncomeItem.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-//import { dateFormat } from '../../utils/dateFormat';
 import { bitcoin, book, calender, card, circle, clothing, comment, dollar, food, freelance, medical, money, piggy, stocks, takeaway, trash, tv, users, yt } from '../../utils/Icons';
 import Button from '../Button/Button';
+import { dateFormat } from '../../utils/dateFormat';
 
 function IncomeItem({
     id,
@@ -74,7 +74,7 @@ function IncomeItem({
                 <div className="inner-content">
                     <div className="text">
                         <p>{dollar} {amount}</p>
-                        <p>{calender} {date}</p>
+                        <p>{calender} {dateFormat(date)}</p>
                         <p>
                             {comment}
                             {description}
