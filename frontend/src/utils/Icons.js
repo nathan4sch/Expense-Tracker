6a39590c87c2c0b3ca4f7e30b71cd6a9f6679a2e
Nathan Schneider <schne165@purdue.edu>
2023-08-03 15:06:01 -0500

    Documentation

--- frontend/src/utils/Icons.js
+++ frontend/src/utils/Icons.js
@@ -19,6 +19,8 @@ export const trash = <i className="fa-solid fa-trash"></i>
 export const signout = <i className="fa-solid fa-right-from-bracket"></i>
 export const takeaway = <i className="fa-solid fa-utensils"></i>
 export const clothing = <i className="fa-solid fa-shirt"></i>
+// Everything from https://fonts.google.com/
+
 export const book = <i className="fa-solid fa-book-open"></i>
 export const medical = <i className="fa-solid fa-briefcase-medical"></i>
 export const tv = <i className="fa-solid fa-tv"></i>
